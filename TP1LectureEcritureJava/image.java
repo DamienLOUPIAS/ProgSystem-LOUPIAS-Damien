d03b7b7fe58b1057b1e47354c8ce374a74705911
DAMIEN LOUPIAS <damien.loupias@iut.local>
2026-09-08 17:51:04 +0200

    save fin scéance

--- TP1LectureEcritureJava/image.java
+++ TP1LectureEcritureJava/image.java
@@ -2,45 +2,39 @@ import java.io.FileWriter;
 import java.io.IOException;
 
 public class Image {
-    public static void main(String[] args) {
 
-        private final int LARGEUR = 3;
-        private final int HAUTEUR = 2;
-        private final int VALEUR_MAX = 255;
+    private int width;
+    private int height;
+    // pixels[y][x][0=R,1=G,2=B]
+    private int[][][] pixels; // pixels[y][x][0=R,1=G,2=B]
 
-        private int width;
-        private int height;
-        // pixels[y][x][0=R,1=G,2=B]
-        private int[][][] pixels; // pixels[y][x][0=R,1=G,2=B]
+    public int getWidth() { return width; }
+    public int getHeight() { return height; }
 
-        public int getWidth() { return width; }
-        public int getHeight() { return height; }
-
-
-        try {
-            FileWriter writer = new FileWriter("FirstPPM.ppm");
-
-           
-
-            writer.write("P3\n");
-            // Écriture des dimensions
-            writer.write(LARGEUR + " " + HAUTEUR);
-            // Écriture de la valeur maximal
-
-            // Écriture des pixels
-            writer.write(VALEUR_MAX);
-
-            // Première ligne : rouge, vert, bleu
-            writer.write("255 0 0 0 255 0 0 0 255");
-
-            // Deuxième ligne : jaune, blanc, noir
-            writer.write("255 255 0 255 255 255 0 0 0");
-
-            writer.close(); // Fermeture du fichier
-
-            System.out.println("Image PPM créée avec succès !");
-        } catch (IOException e) {
-            System.err.println("Erreur lors de l'écriture du fichier : " + e.getMessage());
+    /**
+     * Constructeur : initialise une image vide.
+     */
+    public Image(int width, int height) {
+        this.width = width;
+        this.height = height;
+        pixels = new int[height][width][3];
+    }
+	
+	/**
+     * Définit la couleur d'un pixel à la position (x, y)
+     */
+    public void setPixel(int x, int y, int r, int g, int b) {
+        if (x >= 0 && x < width && y >= 0 && y < height) {
+            pixels[y][x][0] = r;
+            pixels[y][x][1] = g;
+            pixels[y][x][2] = b;
         }
+    }
+	
+    /**
+     * Sauvegarde l'image au format texte PPM (P3)
+     */
+    public void save_txt(String filename) throws IOException {
+        // TODO : écrire le fichier PPM avec FileWriter
     }
 }
