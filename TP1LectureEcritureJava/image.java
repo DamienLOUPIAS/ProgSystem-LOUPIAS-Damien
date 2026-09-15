8090a26112748db53fcefff5d46c4955ac7d3ef2
Damien LOUPIAS <damien.loupias@iut-rodez.fr>
2026-09-15 14:17:00 +0200

    Save non compile

--- TP1LectureEcritureJava/image.java
+++ TP1LectureEcritureJava/image.java
@@ -58,7 +58,7 @@ public class Image {
 				for (int px = 0; px < this.width; px++) {
 				// Génère les pixels
 					for (int valColor = 0; valColor <= 2; valColor++) {
-						// Génère les différente couleur 
+						// Génère les différente couleur / Pas optimum car on appelle plusieurs fois write
 						writer.write(this.pixels[py][px][valColor] + " ");
 						
 					}				
@@ -69,6 +69,72 @@ public class Image {
            
 
             writer.close(); // Fermeture du fichier
+
+        } catch (IOException e) {
+            System.err.println("Erreur lors de l'écriture du fichier : " + e.getMessage());
+        }
+    }
+	
+	
+	/**
+     * Sauvegarde l'image au format texte PPM (P3)
+     */
+    public void save_binaire(String filename) throws IOException {
+		
+		 try {
+            FileWriter writer = new FileWriter(filename);
+			
+			//tableau de byte contenant les bonnes valeur pour construire l'image.
+			byte[] aRetourner = new byte[this.getWidth()*this.getHeight()*3];
+			
+			// Curseur qui nous permet "d'écrire" dans le tableau comme on a fait précédement avec writer
+			int curseur = 0;
+			
+			int = passage;
+		   
+			// header
+            writer.write("P6\n");
+            // Écriture des dimensions
+            writer.write(this.getWidth() + " " + this.getHeight() + "\n");
+            // Écriture de la valeur maximal
+
+            // Écriture des pixels
+            writer.write(VALEUR_MAX + "\n");
+			
+			// Ecriture dans le fichier
+			for (int py = 0; py < this.height; py++) {
+				// Génère les lignes
+				for (int px = 0; px < this.width; px++) {
+				// Génère les pixels
+					for (int valColor = 0; valColor <= 2; valColor++) {
+						// Génère les différente couleur 
+						passage = this.pixels[py][px][valColor];
+						aRetourner[curseur] = (byte) (passage 0xFF)
+						curseur++;
+						
+					}				
+				}
+			}
+			
+			
+			writer.close(); // Fermeture du fichier
+			
+			
+			try (FileOutputStream fos = new FileOutputStream(filename)) {
+                 
+           
+				fos.write(aRetourner);
+
+				System.out.println("Data successfully written to the file.");
+			}
+			catch (IOException e) {
+				System.out.println("An error occurred: ");
+			}
+			
+
+           
+
+        
 
         } catch (IOException e) {
             System.err.println("Erreur lors de l'écriture du fichier : " + e.getMessage());
